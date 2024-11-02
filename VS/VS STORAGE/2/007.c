212704e79f2cf1ec79432db3c07e29f700653166
r0ckmAn1 <113829456+r0ckmAn1@users.noreply.github.com>
2024-11-02 21:58:07 +0530

    5

---
+++ VS/VS STORAGE/2/007.c
@@ -0,0 +1,9 @@
+//Substraction
+#include <stdio.io>
+int main(){
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d",a-b);
+    return 0;
+}
---
+++ VS/VS STORAGE/2/008.c
@@ -0,0 +1,9 @@
+//Multiplication
+#include <stdio.io>
+int main(){
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d",a*b);
+    return 0;
+}
