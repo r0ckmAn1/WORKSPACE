36784e0d461e4302568cc49ce1a0e7a081f3fc3b
r0ckmAn1 <113829456+r0ckmAn1@users.noreply.github.com>
2024-11-04 01:48:13 +0530

    JS update 2

--- VS/VS STORAGE/JavaScript/INDEX2-4.js
+++ VS/VS STORAGE/JavaScript/INDEX2-4.js
@@ -4,9 +4,14 @@ console.log(verX)
 let verY = Math.floor(Math.random()*9)
 console.log(verY)
 let key = verX+verY
-let userKey = prompt(`${verX} + ${verY} = `)
+let userKey = prompt(`verify you are a human (${verX} + ${verY} =)`)
 if (userKey == key)
-console.log("Human Verified")
+    {
+    console.log("Human Verified")
+    console.log("ChapTa support by 511.SEAN")
+    }
 else
-console.log("verification failed")
-    
+        {
+        console.log("verification failed")
+        console.log("XXXXXXXXXXXXXXXXXXX")
+        }
